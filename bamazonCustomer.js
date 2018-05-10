f5b27d1807826ef718755c71c8801681a5d8db73
Matthew Buchanan <matt@matthews-air.home>
2018-05-10 17:53:07 -0400

    console logs products in SQL table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var keys = require('./keys.js');
+
 
 var connection = mysql.createConnection({
-  host: keys.host,
-  port: keys.port,
-  user: keys.user,
-  password: keys.password,
-  database: keys.database
+host: 'localhost',
+port: 3306,
+user: 'root',
+password: 'password',
+database: 'bamazon'
 });
+
+function displayProducts() {
+  connection.query('SELECT * FROM products', (err, results) => {
+    if (err) throw err;
+	console.log('---------------------');
+    console.log(' WELCOME TO BAMAZON!');
+	console.log('---------------------');
+    results.forEach(product => {
+      console.log(`| ID: ${product.item_id} | Name: ${product.product_name} | Price: ${product.price} |`);
+	});
+    connection.end(function(err){});
+  });
+}
+
 
 
