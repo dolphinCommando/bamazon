2396f791c00703b0ee1d189b4e8e73c5fa0f0c64
Matthew Buchanan <matt@matthews-air.home>
2018-05-10 00:01:21 -0400

    initial files and a SQL schema

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(50) NULL,
+    price FLOAT(10, 2) NULL,
+    stock_quantity INTEGER(12) NULL,
+    product_sales FLOAT(14,2) NULL,
+	PRIMARY KEY (item_id)
+);
+
+CREATE TABLE departments (
+	department_id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(50) NULL,
+    over_head_costs FLOAT(12,2) NULL,
+    PRIMARY KEY (department_id)
+);
+
+SELECT * FROM products;
+SELECT * FROM departments;
