90fcc3bc74d1421f51d06985b68e37d7f3942eeb
Matthew Buchanan <matt@matthews-air.home>
2018-05-12 00:50:56 -0400

    CLI Customer app displays products in SQL table, takes inquirer input, and updates SQL products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,9 +20,67 @@ function displayProducts() {
     results.forEach(product => {
       console.log(`| ID: ${product.item_id} | Name: ${product.product_name} | Price: ${product.price} |`);
 	});
-    connection.end(function(err){});
+  
+	getProductID(results);
   });
 }
+
+function getProductID(arr) {
+	inquirer.prompt([
+		{
+			name: 'id',
+			message: 'Choose the ID of the product you wish to buy.',
+			type: 'input'
+		}
+	]).then(response => {
+		if (+response.id > 0 && +response.id <= arr.length && parseInt(response.id) !== NaN) {
+			getProductUnits(arr, response.id);
+		}
+		else {
+			getProductID(arr);
+		}
+
+	});
+}
+
+function getProductUnits(arr, index_id) {
+	inquirer.prompt([
+		{
+			name: 'units',
+			message: 'How many do you want to order?',
+			type: 'input'
+		}
+	]).then(response => {
+      if (!Number.isInteger(+response.units)) {
+		  getProductUnits(arr);
+	  }
+	  else {
+		  if (arr[+index_id - 1].stock_quantity < +response.units) {
+			  console.log('Sorry, insufficient quantity.');
+			  console.log('Returning to main page.');
+			  setTimeout(() => {
+				  displayProducts();
+			  }, 1500);
+		  }
+		  else {
+			  placeOrder(arr, index_id, response.units);
+		  }
+	  }
+	});
+}
+
+function placeOrder(arr, id, quantity) {
+	var someMath = +arr[+id-1].price * +quantity;
+	var stockNow = +arr[+id-1].stock_quantity - +quantity;
+	connection.query('UPDATE products SET stock_quantity = ?, product_sales = ? WHERE item_id = ?', [stockNow, someMath, id], (err, results) => {
+		if (err) throw err;
+		console.log(`Your bank account has been billed $${someMath}.\nThank you for ordering through BAMAZON.`);
+		  connection.end(function(err){});
+	});
+}
+
+displayProducts();
+
 
 
 
